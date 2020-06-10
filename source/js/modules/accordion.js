acb2b41ed900ca7376bd018e3bb150bf7eaa6bc3
viktor <viktorprodeus@gmail.com>
2020-06-10 14:18:06 +0300

    исправил замечания по баг-листу

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -1,41 +1,36 @@
-const buttonNav = document.querySelector(`.main-footer__toggle-btn--nav`);
-const buttonContact = document.querySelector(`.main-footer__toggle-btn--contacts`);
 const blockNav = document.querySelector(`.main-footer__navigation`);
 const blockContact = document.querySelector(`.main-footer__contacts`);
 
-const hideBlock = (block, btn) => {
-  if (btn) {
-    block.classList.remove(`open-js`);
-  }
+const hideBlock = (block) => {
+  block.classList.remove(`open-js`);
 };
 
-const showBlock = (block, btn) => {
-  if (btn) {
-    block.classList.add(`open-js`);
-  }
+
+const showBlock = (block) => {
+  block.classList.add(`open-js`);
 };
 
-if (buttonNav) {
-  buttonNav.addEventListener(`click`, (evt) => {
-    evt.preventDefault();
-    if (blockNav.classList.contains(`open-js`)) {
-      hideBlock(blockNav, buttonNav);
 
-    } else {
-      showBlock(blockNav, buttonNav);
-      hideBlock(blockContact, buttonContact);
-    }
-  });
-}
+blockNav.addEventListener(`click`, (evt) => {
+  evt.preventDefault();
 
-if (buttonContact) {
-  buttonContact.addEventListener(`click`, (evt) => {
-    evt.preventDefault();
-    if (blockContact.classList.contains(`open-js`)) {
-      hideBlock(blockContact, buttonContact);
-    } else {
-      showBlock(blockContact, buttonContact);
-      hideBlock(blockNav, buttonNav);
-    }
-  });
-}
+  if (blockNav.classList.contains(`open-js`)) {
+    hideBlock(blockNav);
+
+  } else {
+    showBlock(blockNav);
+    hideBlock(blockContact);
+  }
+});
+
+
+blockContact.addEventListener(`click`, (evt) => {
+  evt.preventDefault();
+
+  if (blockContact.classList.contains(`open-js`)) {
+    hideBlock(blockContact);
+  } else {
+    showBlock(blockContact);
+    hideBlock(blockNav);
+  }
+});
